e84f01d24e35e18e5fb8ac9a892bb1d4d37d04bf
Bruno Moyse <cloud@nuagemagique.dev>
2026-04-25 17:14:59 +0200

    fix: allow blob: URLs in CSP and use native fetch for image preview

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,7 @@ const csp = `${[
     "default-src 'self'",
     "script-src 'self' 'unsafe-inline'",
     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-    `img-src 'self' data:${s3Url ? ` ${s3Url}` : ''}`,
+    `img-src 'self' data: blob:${s3Url ? ` ${s3Url}` : ''}`,
     "font-src 'self' https://fonts.gstatic.com",
     `connect-src 'self' ${apiOrigin} ${wsOrigin} ${zitadelOrigin} https://api.iconify.design`,
     `frame-src 'self' ${zitadelOrigin}`,
